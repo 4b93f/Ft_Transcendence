e6afa25bc777a2e118f3c919d1b290879f108d0c
Charli Ly huchard <chly-huc@fu-r7-p1.s19.be>
2022-11-25 18:11:00 +0100

    paddle are moving :)

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -18,6 +18,9 @@ const u1 = {
     y : 0,
     width : 50,
     height : 250,
+    speed : 20,
+    max : 0,
+    min : 0,
     score : 0,
     color : "#FFFFFF"
 }
@@ -26,6 +29,7 @@ const u2 = {
     x : 0,
     y : 0,
     width : 50,
+    speed : 10,
     height : 250,
     score : 0,
     color : "#FFFFFF"
@@ -39,6 +43,22 @@ type CanvasProps = {
 function DrawRec(x: number, y: number, w: number, h: number, color:string){
     context.fillStyle = color;
     context.fillRect(x, y, w , h);
+}
+
+function PaddleUp(){
+    context.fillStyle = '#000000';
+    context.fillRect(u1.x, u1.y, u1.width, u1.height);
+    if (u1.y - u1.speed >= -10)
+        u1.y -= u1.speed;
+}
+
+function PaddleDown(){
+    context.fillStyle = '#000000';
+    context.fillRect(u1.x, u1.y, u1.width, u1.height)
+    if (u1.y + u1.speed + u1.height <= u1.max + 10)
+        u1.y += u1.speed;
+    console.log(u1.y);
+    console.log(u1.max);
 }
 
 function DrawBall(x: number, y: number, r: number, color:string){
@@ -89,8 +109,21 @@ const Canvas = (props:CanvasProps) => {
                 u2.y = (canvas.height - u2.height) / 2 ;
                 u2.x = canvas.width - u2.width;
                 ball.y = canvas.height /2;
+                u1.max = canvas.height;
                 setInterval(render, 10);
             }
+            canvas.tabIndex = 1;
+            canvas.addEventListener('keydown', function(e:any) {
+                if (e.keyCode === 38){
+                    console.log("UP");
+                    PaddleUp();
+                }
+                if (e.keyCode === 40){
+                    console.log("DOWN");
+                    PaddleDown();
+                }
+
+            });
         }
     })
     return <canvas ref={canvasRef} height={props.height} width={props.width}/>;
